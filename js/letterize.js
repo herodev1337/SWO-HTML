88ffdc38338d3f71dc880c5c6829a8db04199f9b
Heichel <benno.heichel@softwareone.com>
2021-09-10 15:12:26 +0200

    4rd Day | Pretty much done

---
+++ js/letterize.js
@@ -0,0 +1,14 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  });
