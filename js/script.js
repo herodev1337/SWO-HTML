1a164abc04ac66854aa47b25ac37e8db9ad8c461
Heichel <benno.heichel@softwareone.com>
2021-09-15 15:49:36 +0200

    Really last commit. I think

--- js/script.js
+++ js/script.js
@@ -1,89 +1,168 @@
-/* LETTERIZE START */
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////// MAIN ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Sets the padding because of the Navbar
+$(document).ready(function() {
+    $('.main-container').css('padding-top', $('nav').css("height"));
+});
+
+//Makes the Navbar look cool
+window.onscroll = function() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $('*[class^="nav-caption"]').each(function(index) {
+            $(this).addClass('nav-shrink');
+            $(this).removeClass('nav-grow');
+        });
+        $('.main-container').css('padding-top', parseInt($('nav').css("height"))-30);
+    } else {
+        $('*[class^="nav-caption"]').each(function(index) {
+            $(this).addClass('nav-grow');
+            $(this).removeClass('nav-shrink');
+        });
+        $('.main-container').css('padding-top', $('nav').css("height"));
+    }
+    //
+    //$('.main-container').css('padding-top', $('nav').css("height"));
+};
+
+//Activates the Bootstrap scrollspy
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar'
+});
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////// LETTERIZE /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Input: None
+Output: None
+Action: Sets the animation for the Letters on the Home Screen
+*/
+
 
 var textWrapper = document.querySelector('.letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: false})
-  .add({
-    targets: '.letter',
-    scale: [0.3,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 600,
-    delay: (el, i) => 70 * (i+1)
-  });
+anime.timeline({ loop: false })
+    .add({
+        targets: '.letter',
+        scale: [0.3, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 600,
+        delay: (el, i) => 70 * (i + 1)
+    });
 
-/* LETTERIZE END */
 
-/* INVIEWPORT START */
+/*------------------------------------------------------------------------------------------------*/
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////// PROGRESSBAR ////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Looks if element is in viewport
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.isIntersecting) {
-          if($(entry.target).attr('aria-loaded')) return;
-          
-          var bar = new ProgressBar.SemiCircle(entry.target, {
-              strokeWidth: 6,
-              color: '#ED6A5A',
-              trailColor: '#eee',
-              trailWidth: 1,
-              easing: 'easeInOut',
-              duration: 2000,
-              svgStyle: null,
-              text: {
-                  value: '',
-                  alignToBottom: false
-              },
-              from: {color: '#ED6A5A'},
-              to: {color: '#32CD32'},
-              
-              step: (state, bar) => {
-                  bar.path.setAttribute('stroke', state.color);
-                  var value = Math.round(bar.value() * 100);
-                  if (value === 0) {
-                      bar.setText('');
-                  } else {
-                      bar.setText(value);
-                  }
-              
-                  bar.text.style.color = state.color;
-              }
-          });
-  
-          bar.animate($(entry.target).attr('aria-valuenow')/100);
-          $(entry.target).attr('aria-loaded', true);
-        return;
-      }
+        if (entry.isIntersecting) {
+            //In viewport? Animate!
+            animateProgressbar(entry.target);
+            return;
+        }
     });
-  });
-  
-  $('*[id^="progress-animated-"]').each(function(i, obj) {
-      observer.observe(document.getElementById($(this)[0].id));
-  });
+});
 
-/* INVIEWPORT END */
+//Array to save the progressbar elements
+let pgb = [];
 
-/* RESIZENAVBAR START */
+/*
+Input: Element
+Output: none
+Action: Initialize the Progressbar and starts to observe it. When called again and the element is already initialized, it starts the animation if in viewport.
+*/
+function animateProgressbar(element) {
+    //Index Variable for Array
+    let i;
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $('*[class^="section"]').each(function( index ) {
-            $(this).addClass('d-none');
-            $('section').css('padding-top', $('nav').css("height"));
-            $('section').css('height', $(window).height() - $('nav').css("height"));
-          });
-    } else {
-        $('*[class^="section"]').each(function( index ) {
-            $(this).removeClass('d-none');
-          });
+    //Already Initialized? If not ->
+    if (!$(element).attr('aria-init')) {
+        console.log("Element %s not initialized. Pushing to pgb...", element);
+
+        //Push the Progressbar element into the Array
+        pgb.push(new ProgressBar.SemiCircle(element, {
+            strokeWidth: 6,
+            color: '#ED6A5A',
+            trailColor: '#eee',
+            trailWidth: 1,
+            easing: 'easeInOut',
+            duration: 2000,
+            svgStyle: null,
+            text: {
+                value: '',
+                alignToBottom: false
+            },
+            from: { color: '#ED6A5A' },
+            to: { color: '#32CD32' },
+
+            step: (state, bar) => {
+                bar.path.setAttribute('stroke', state.color);
+                var value = Math.round(bar.value() * 100);
+                if (value === 0) {
+                    bar.setText('');
+                } else {
+                    bar.setText(value);
+                }
+
+                bar.text.style.color = state.color;
+            }
+        }));
+
+        //Set the element attr "aria-init" to true
+        $(element).attr('aria-init', true);
+        console.log("Pushed successfully. Starting to observe...");
+
+        //Lets observe the element
+        observer.observe(element);
+        return;
+    } else { //<- Already initialized
+        //Check all entries in Array to get the matching ID
+        pgb.forEach((arrayitem, index) => {
+            if ($(arrayitem["_container"]).attr("id").match($(element).attr('id'))) {
+                console.log("Got element %s -> Container %s with Index %s", $(element).attr('id'), $(arrayitem["_container"]).attr("id"), index)
+                i = index;
+            }
+        });
+
+        //Already loaded? If true, return. If not, go on
+        if ($(element).attr('aria-loaded')) { return; }
+
+        //Get the Element from the Array and let it animate. Value from "aria-valuenow"/100
+        pgb[i].animate($(element).attr('aria-valuenow') / 100);
+
+        //Set "aria-loaded" to true
+        $(element).attr('aria-loaded', true);
+        console.log("Successfully animated %s with ID %s", element, i)
+        return;
     }
-    //$('section').css('padding-top', $('nav').css("height"));
-};
+}
 
-/* RESIZENAVBAR END */
+//Get all Elements with "progress-animated-*" and let the observer watch them
+$('*[id^="progress-animated-"]').each(function(i, obj) {
+    animateProgressbar(document.getElementById($(this)[0].id), i);
+});
 
-/* PARTICLEJS START */
+
+/*------------------------------------------------------------------------------------------------*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////// PARTICLE.JS ////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Initialize the particle engine
 particlesJS('particle-js',
   
 {
@@ -194,9 +273,4 @@ particlesJS('particle-js',
 
 );
 
-/* PARTICLEJS END*/
-
-/* SMOOTH SCROLLING */
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar'
-});
+/*------------------------------------------------------------------------------------------------*/
