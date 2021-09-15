e7a01aa3a7d20ba07966ece4b1451f579b532362
Heichel <benno.heichel@softwareone.com>
2021-09-15 14:36:57 +0200

    Should be done. testing...

--- js/ns.js
+++ js/ns.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 window.onscroll = function() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         $('*[class^="nav-caption"]').each(function(index) {
-            $(this).addClass('d-none');
+            $(this).addClass('nav-shrink');
         });
     } else {
         $('*[class^="nav-caption"]').each(function(index) {
-            $(this).removeClass('d-none');
+            $(this).removeClass('nav-shrink');
         });
     }
     $('.main-container').css('padding-top', $('nav').css("height"));
@@ -136,3 +136,116 @@ $('*[id^="progress-animated-"]').each(function(i, obj) {
 });
 
 /*------------------------------------------------------------------------------------------------*/
+
+/* PARTICLEJS START */
+particlesJS('particle-js',
+  
+{
+    "particles": {
+      "number": {
+        "value": 5,
+        "density": {
+          "enable": true,
+          "value_area": 80
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+);
+
+/* PARTICLEJS END*/
